196691cfe80e965c57382017313f8ac60455d5e5
Andrei Roh <andrei_roh@tut.by>
2021-06-20 23:38:59 +0300

    refactor: update code
    Fix linter errors

--- src/common/config.ts
+++ src/common/config.ts
@@ -5,6 +5,7 @@ dotenv.config({ path: path.join(__dirname, '../../.env'), });
 
 export const {
   PORT,
+  AUTH_MODE,
   NODE_ENV,
   JWT_SECRET_KEY,
   POSTGRES_DBHOST,
@@ -12,5 +13,4 @@ export const {
   POSTGRES_USER,
   POSTGRES_PASSWORD,
 } = process.env;
-export const POSTGRES_PORT = Number(process.env['POSTGRES_PORT']);
-export const AUTH_MODE = process.env['AUTH_MODE'] === 'true';
+export const POSTGRES_PORT = Number(process.env);
